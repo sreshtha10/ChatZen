6b77131eafb3332c2b7f743cd06eb542256e2e40
Sreshtha <sreshtha.mehrotra@gmail.com>
2022-04-09 11:40:01 +0530

    created home act. pt2

--- app/src/main/java/com/sreshtha/chatappandroid/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/sreshtha/chatappandroid/ui/activities/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.sreshtha.chatappandroid.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sreshtha.chatappandroid.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
