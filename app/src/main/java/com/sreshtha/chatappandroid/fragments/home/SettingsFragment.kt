8aaf14608a5189da189b64fb2902d5a3b17a5782
Sreshtha <sreshtha.mehrotra@gmail.com>
2022-04-13 03:48:16 +0530

    added cust alert diaglog, impl. change nickname fn

--- app/src/main/java/com/sreshtha/chatappandroid/fragments/home/SettingsFragment.kt
+++ app/src/main/java/com/sreshtha/chatappandroid/fragments/home/SettingsFragment.kt
@@ -1,6 +1,7 @@
 package com.sreshtha.chatappandroid.fragments.home
 
 import android.Manifest
+import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -11,15 +12,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 import com.google.firebase.storage.FirebaseStorage
+import com.sreshtha.chatappandroid.R
 import com.sreshtha.chatappandroid.activities.HomeActivity
 import com.sreshtha.chatappandroid.activities.MainActivity
 import com.sreshtha.chatappandroid.databinding.FragmentSettingsBinding
@@ -80,6 +85,8 @@ class SettingsFragment:Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initValues(view)
+
         settingsBinding?.apply {
 
             llLogout.setOnClickListener {
@@ -89,8 +96,6 @@ class SettingsFragment:Fragment() {
                 (activity)?.finish()
             }
 
-
-            Glide.with(view).load(mViewModel.currentUser.photoUrl).into(profileImage)
 
             llChangeProfilePic.setOnClickListener {
                 if(!mViewModel.hasInternetConnection()){
@@ -109,6 +114,11 @@ class SettingsFragment:Fragment() {
 
             }
 
+
+            llChangeNickname.setOnClickListener {
+                displayCustomAlert()
+            }
+
         }
 
     }
@@ -119,9 +129,6 @@ class SettingsFragment:Fragment() {
     }
 
 
-    private fun setupStorage(){
-        val storageRef = storage.reference
-    }
 
     private fun hasReadExternalStoragePermission(context: Context):Boolean{
         return ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
@@ -173,6 +180,51 @@ class SettingsFragment:Fragment() {
                     Toast.makeText(activity,"Cannot Update Profile Picture!",Toast.LENGTH_SHORT).show()
                     Log.d(TAG,it.toString())
                 }
+        }
+    }
+
+    private fun displayCustomAlert(){
+        val custView = layoutInflater.inflate(R.layout.alert_edit_box,null)
+        val alertDialog = AlertDialog.Builder(requireContext()).create()
+        alertDialog.setCancelable(false)
+
+        alertDialog.setView(custView)
+        alertDialog.show()
+
+
+        val btnCancel = custView.findViewById<Button>(R.id.btn_cancel)
+        val btnOk = custView.findViewById<Button>(R.id.btn_ok)
+
+        btnCancel.setOnClickListener {
+            alertDialog.cancel()
+        }
+
+        btnOk.setOnClickListener {
+            val nickname = custView.findViewById<TextInputEditText>(R.id.et_nickname).text.toString()
+            //update nickname
+            if(!mViewModel.hasInternetConnection()){
+                Snackbar.make(requireView(),"No Internet Connection!",Snackbar.LENGTH_SHORT).show()
+                alertDialog.cancel()
+                return@setOnClickListener
+            }
+            settingsBinding?.dotLoader?.visibility = View.VISIBLE
+            val profileUpdates = UserProfileChangeRequest.Builder().setDisplayName(nickname).build()
+            mViewModel.currentUser.updateProfile(profileUpdates)
+            settingsBinding?.dotLoader?.visibility = View.GONE
+            settingsBinding?.tvUserName?.text = nickname
+            alertDialog.cancel()
+
+        }
+
+    }
+
+
+
+    private fun initValues(view: View){
+        settingsBinding?.apply {
+            tvUserEmail.text = mViewModel.currentUser.email
+            tvUserName.text = mViewModel.currentUser.displayName
+            Glide.with(view).load(mViewModel.currentUser.photoUrl).into(profileImage)
         }
     }
 }
